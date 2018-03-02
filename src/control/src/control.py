aa533f7f1abd5f653311b23ceceb22cf74ab625b
Suchat Tangjarukij <suchat.tangjarukij@gmail.com>
2018-03-02 07:19:53 +0700

    robot refractor

--- src/control/src/control.py
+++ src/control/src/control.py
@@ -38,38 +38,51 @@ class joint_publisher:
         self.pub.publish(self.message)
         
 
+class robotIK:
+    def __init__(self,urdf_path,home):
+        self.chain = ikpy.chain.Chain.from_urdf_file(urdf_path)
+        self.home = home
+
+        # target_vector = [ 0, 0.25, 0.4]
+        target_frame = np.eye(4)
+        target_frame[:3, 3] = home
+        self.current_joint = self.chain.inverse_kinematics(target_frame)
+
+    def forwardIK(self,joint):
+
+        return self.chain.forward_kinematics(joint)
+
+    def inverseIK(self,pose,current_joint=None):
+
+        target_frame = np.eye(4)
+        target_frame[:3, 3] = pose
+
+        if current_joint is None:
+            target_joint = self.chain.inverse_kinematics(target_frame)
+        else:
+            target_joint = self.chain.inverse_kinematics(target_frame,joint)
+
+        return target_joint
 
 
-# from ikpy import plot_utils
-rp = rospkg.RosPack()
-script_path = os.path.join(rp.get_path("control"), "urdf", "kuka_bot.urdf")
 
-my_chain = ikpy.chain.Chain.from_urdf_file(script_path)
 
 #initialize stuffs
 
 VID = virtual_impedance(3.0,15.0,0.5)
 pub = joint_publisher()
 
+# URDFIK
+rp = rospkg.RosPack()
+urdf_path = os.path.join(rp.get_path("control"), "urdf", "kuka_bot.urdf")
+robot = robotIK(urdf_path,[ 0, 0.25, 0.4])
+joint = robot.current_joint
 
-
-#starting pose
-target_vector = [ 0, 0.25, 0.4]
-target_frame = np.eye(4)
-target_frame[:3, 3] = target_vector
-
-joint = my_chain.inverse_kinematics(target_frame)
-real_frame = my_chain.forward_kinematics(joint)
-
-starting_pos = real_frame[:3, 3]
 
 #simulation configuration
-force = np.array([0.0,3.0,3.0])
+force = np.array([3.0,3.0,3.0])
 dt = 0.132 # 132ms
-iteration = 10000
-displace = np.zeros([iteration,3])
-joints = np.zeros([iteration,7])
-_positions = np.zeros([iteration,3])
+iteration = 1000
 
 # the loop
 
@@ -78,34 +91,13 @@ for i in range(iteration):
 
     #virtual impedance
     dis = VID.run(force,dt)
-    
-    target_pos = target_vector + dis
-    target_frame = np.eye(4)
-    target_frame[:3, 3] = target_pos
-    joint = my_chain.inverse_kinematics(target_frame,joint)
-    joints[i] = joint
 
+    joint = robot.inverseIK(robot.home + dis,joint)
 
     #######execute to robot here#######
     pub.send(joint)
     ###################################
 
-    real_frame = my_chain.forward_kinematics(joints[i])
-    current_pos = real_frame[:3, 3]
-    _positions[i] = current_pos
-    
     dt = time.time() - t
 
     print("total elapse time : ",dt )
-
-
-
-# fig = plt.figure()
-# ax = fig.add_subplot(311)
-# ax.plot(range(iteration), displace)
-# ax1 = fig.add_subplot(312)
-# ax1.plot(range(iteration), joints)
-# ax2 = fig.add_subplot(313)
-# ax2.plot(range(iteration), _positions)
-
-# plt.show()
